ec0eedea79ab3bf2453d2ad5e6da5a99248976c0
melvin taider <tmneo1989@hotmail.com>
2023-07-30 13:29:33 +0200

    Initial commit

---
+++ app/src/main/java/be/melyuki/demo02_ui/MainActivity.kt
@@ -0,0 +1,11 @@
+package be.melyuki.demo02_ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
